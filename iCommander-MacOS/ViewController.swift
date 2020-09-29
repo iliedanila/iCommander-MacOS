929ee6abfe586a91f5af87a2db158bbfac1969ee
Ilie Danila <danila.ilie@gmail.com>
2020-09-29 21:34:08 +0300

    Initial Commit

---
+++ iCommander-MacOS/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  iCommander-MacOS
+//
+//  Created by Ilie Danila on 29/09/2020.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
