b3009fbfbaad4f70bea767ead1fa4b832b627ed7
Ilie Danila <danila.ilie@gmail.com>
2021-02-27 02:21:16 +0200

    First draft of search window

---
+++ iCommander/Controllers/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  iCommander
+//
+//  Created by Ilie Danila on 27.02.2021.
+//
+
+import Cocoa
+
+class SearchViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ iCommander/Controllers/SearchWindowController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchWindowController.swift
+//  iCommander
+//
+//  Created by Ilie Danila on 27.02.2021.
+//
+
+import Cocoa
+
+class SearchWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+
+}
